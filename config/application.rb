d890d331232be3c6e251956b9b996e11b972587d
s.golovko <s.golovko@fun-box.ru>
2020-06-18 21:41:28 +0500

    Правки по ревью.

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ Bundler.require(*Rails.groups)
 module TestGuru
   class Application < Rails::Application
     config.load_defaults 6.0
-    config.time_zone = 'Asia/Yekaterinburg'
+    config.time_zone = 'Ekaterinburg'
     config.i18n.default_locale = :ru
   end
 end

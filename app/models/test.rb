4122dd5e981360b4be01bf8934a348423c913c35
s.golovko <s.golovko@fun-box.ru>
2020-06-20 00:04:37 +0500

    Правки по ревью.

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,7 @@
 class Test < ApplicationRecord
   def self.titles_by_category(title:)
-    select(:title)
-      .joins('JOIN categories ON tests.category_id = categories.id')
-      .where('categories.title = ?', title)
+    joins('JOIN categories ON tests.category_id = categories.id')
+      .where(categories: { title: title })
       .order('tests.title DESC')
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def tests_by_level(level)
     Test.joins('JOIN statistics ON tests.id = statistics.test_id')
-        .where('statistics.user_id = :id AND tests.level = :level', id: id, level: level)
+        .where(tests: { level: level }, statistics: { user_id: id })
   end
 end

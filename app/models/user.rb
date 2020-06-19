95ee6670e0c4dcc0e4953b2735a6bea4bba8305c
s.golovko <s.golovko@fun-box.ru>
2020-06-19 15:33:29 +0500

    Добавлены методы выборки данных.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    Test.joins('JOIN statistics ON tests.id = statistics.test_id')
+        .where('statistics.user_id = :id AND tests.level = :level', id: id, level: level)
+  end
 end

cc68ba4cf7aa026011e57ba5855708af98b56603
s.golovko <s.golovko@fun-box.ru>
2020-06-21 20:46:16 +0500

    Вернул метод вместо скоупа.

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,10 @@ class Test < ApplicationRecord
   has_many :statistics, dependent: :destroy
   has_many :users, through: :statistics
 
-  scope :titles_by_category, lambda { |title|
-    Category.find_by(title: title)
-            .tests
-            .order('title DESC')
-            .pluck(:title)
-  }
+  def self.titles_by_category(title:)
+    joins(:category)
+      .where(categories: { title: title })
+      .order('tests.title DESC')
+      .pluck(:title)
+  end
 end

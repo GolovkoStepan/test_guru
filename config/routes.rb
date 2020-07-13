c150fb102b38a082c4e398a1a2c16e73e4774799
s.golovko <s.golovko@fun-box.ru>
2020-07-13 22:01:24 +0500

    Правки по ревью.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get '/sign_up', to: 'users#new'
   get '/sign_in', to: 'sessions#new'
-  post '/sign_out', to: 'sessions#destroy'
+  delete '/sign_out', to: 'sessions#destroy'
 
   resources :users, only: :create
   resources :sessions, only: :create

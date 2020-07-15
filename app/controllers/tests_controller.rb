5a3884330e442e455ea1c99b987cf1680f6f6d89
GolovkoStepan <stepagolovko1993@gmail.com>
2020-07-15 22:02:40 +0500

    Установка devise.

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,5 +1,5 @@
 class TestsController < ApplicationController
-  skip_before_action :auth_user!, only: :index
+  skip_before_action :authenticate_user!, only: :index
   before_action :find_test, only: %i[show edit update destroy start]
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,39 +1,46 @@
-# frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: users
 #
-#  id              :bigint           not null, primary key
-#  email           :string           not null
-#  first_name      :string           not null
-#  last_name       :string           not null
-#  middle_name     :string
-#  password_digest :string           not null
-#  role            :integer          default("regular")
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
+#  id                     :bigint           not null, primary key
+#  confirmation_sent_at   :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string
+#  last_name              :string
+#  middle_name            :string
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  role                   :integer          default("regular")
+#  unconfirmed_email      :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
 #
 # Indexes
 #
-#  index_users_on_email  (email) UNIQUE
+#  index_users_on_confirmation_token    (confirmation_token) UNIQUE
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
-
 class User < ApplicationRecord
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :validatable,
+         :confirmable
+
   has_many :statistics, dependent: :destroy
   has_many :tests, through: :statistics
   has_many :my_tests, class_name: 'Test', foreign_key: 'user_id', dependent: :destroy
 
-  has_secure_password
-
-  validates :first_name, :last_name, :email, presence: true
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates_uniqueness_of :email
-
   enum role: %i[admin regular]
 
   def username
-    [first_name, last_name].map(&:capitalize).join(' ')
+    [first_name, last_name].map(&:capitalize).join(' ') rescue "username"
   end
 
   def tests_by_level(level)
